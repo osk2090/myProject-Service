45a03905936b0d6932d8aef90d6ec0f2232cf97a
osk2090 <osk2090@gmail.com>
2021-01-25 20:02:26 +0900

    .

--- app/src/main/java/com/osk2090/Draw/Admin.java
+++ app/src/main/java/com/osk2090/Draw/Admin.java
@@ -1,6 +1,8 @@
 package com.osk2090.Draw;
 
+import com.osk2090.Draw.util.AdminCheck;
 import com.osk2090.Draw.util.InputDrawInfo;
+import com.osk2090.Draw.util.Prompt;
 
 import java.util.Random;
 
@@ -22,5 +24,25 @@ public class Admin {
         int n = ran.nextInt(InputDrawInfo.idx);
         System.out.println("당첨자:" + InputDrawInfo.clients[n].name);
         System.out.println("축하합니다!");
+    }
+
+    public static void AdminLogic() {
+        if (AdminCheck.checkResult()) {
+            boolean run = true;
+            int choice = Prompt.promptInt("1. 추첨하기 2. 응모자리스트 3. 로그아웃");
+            while (run) {
+                if (choice == 1) {
+                    System.out.println("추첨하겠습니다.");
+                    Admin.WinnerResult();
+                } else if (choice == 2) {
+                    Admin.AdminPrintList();
+                } else if (choice == 3) {
+                    System.out.println("로그아웃 되었습니다.");
+                    break;
+                } else {
+                    System.out.println("없는 메뉴 입니다.");
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/osk2090/Draw/Draw.java
+++ app/src/main/java/com/osk2090/Draw/Draw.java
@@ -1,6 +1,5 @@
 package com.osk2090.Draw;
 
-import com.osk2090.Draw.util.AdminCheck;
 import com.osk2090.Draw.util.InputDrawInfo;
 import com.osk2090.Draw.util.Prompt;
 
@@ -26,23 +25,7 @@ public class Draw {
                     System.out.println("다시 동의하시기 바랍니다.");
                 }
             } else if (choice == 2) {
-                    if (AdminCheck.checkResult()) {
-                        run = true;
-                        choice = Prompt.promptInt("1. 추첨하기 2. 응모자리스트 3. 로그아웃");
-                        while (run) {
-                            if (choice == 1) {
-                                System.out.println("추첨하겠습니다.");
-                                Admin.WinnerResult();
-                            } else if (choice == 2) {
-                                Admin.AdminPrintList();
-                            } else if (choice == 3) {
-                                System.out.println("로그아웃 되었습니다.");
-                                break;
-                            } else {
-                                System.out.println("없는 메뉴 입니다.");
-                            }
-                        }
-                    }
+                Admin.AdminLogic();
             } else {
                 System.out.println("다시 선택해주세요.");
             }
